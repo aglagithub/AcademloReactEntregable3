2732a000194fd0caaf74236060fc9af0354a35b7
Andres Lara <angillama@gmail.com>
2023-06-03 06:23:39 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-indigo-300"> Entregable 3</h1>
+    </>
+  );
+}
+
+export default App;
